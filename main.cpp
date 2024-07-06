4b5bf5408492b1256119a713d9bd6d2666597912
Fábio Marques <2fabiom@gmail.com>
2024-07-07 00:36:17 +0100

    v0

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+//PADEL: um set são 6 jogos
+
+class JogoPadel {
+    int pontosA;
+    int pontosB;
+
+public:
+     JogoPadel () {
+        cout << "novo jogo";
+        pontosA = 0;
+        pontosB = 0;
+    }
+    void start(){
+        cout << "inicia";
+    }
+};
+
+class SetPadel {
+    vector<JogoPadel> jogos;
+
+  public:
+    SetPadel () {
+        cout << "novo Set";
+        JogoPadel jogo;
+        jogo.start();
+        jogos.push_back(jogo);
+    }
+};
+
+int main(){
+    SetPadel padel;
+
+}
